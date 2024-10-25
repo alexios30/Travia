d4150daf97c4c2b21108db4c10b1b89c49fc1930
alexios30 <alexischampault.travail@gmail.com>
2024-10-25 12:12:40 +0200

    add import ships (à tester)

--- class/ship.php
+++ class/ship.php
@@ -92,6 +92,32 @@ class Ship {
      * @param mixed $id
      */
 
+    public function add_ships($json) {
+        include("./include/connexion.php");
+
+        try {
+            $vaisseauxData = json_decode($json, true);
+            $stmt = $cnx->prepare("INSERT INTO Ship (id, name, camp, speed_kmh, capacity) VALUES (:id, :name, :camp, :speed_kmh, :capacity)");
+
+            foreach ($vaisseauxData as $vaisseau) {
+                // Lier chaque paramètre à sa valeur avec un type explicite
+                $stmt->bindParam(':id', $vaisseau['id'], PDO::PARAM_INT);
+                $stmt->bindParam(':name', $vaisseau['name'], PDO::PARAM_STR);
+                $stmt->bindParam(':camp', $vaisseau['camp'], PDO::PARAM_STR);
+                $stmt->bindParam(':speed_kmh', $vaisseau['speed_kmh'], PDO::PARAM_STR);
+                $stmt->bindParam(':capacity', $vaisseau['capacity'], PDO::PARAM_INT);
+
+                // Exécuter la requête d'insertion
+                $stmt->execute();
+            }
+
+            echo "Données insérées avec succès !";
+
+        } catch (PDOException $e) {
+            echo "Erreur de connexion : " . $e->getMessage();
+        }
+    }
+
 
 
 }
--- scripts/import_ships.php
+++ scripts/import_ships.php
@@ -1,3 +1,5 @@
 <?php
-
+    $json = file_get_contents('../data/ships.json');
+    $add_json_bdd = new Ship();
+    $add_json_bdd->add_ships($json)
 ?>
