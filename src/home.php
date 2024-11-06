20863b2d08bdc49ea36e25fa8270974b61d59a3f
alexios30 <alexischampault.travail@gmail.com>
2024-11-06 09:54:04 +0100

    Moove scrip js

--- src/home.php
+++ src/home.php
@@ -37,45 +37,6 @@
 <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
-<script>
-    $(document).ready(function() {
-        // Charger les planètes depuis le back-end PHP
-        $.getJSON("../sql/AutoPlanet.php", function(data) {
-            // Vérifier si les données sont valides
-            if (data && Array.isArray(data)) {
-                // Activer l'auto-complétion pour les champs "départ" et "arrivée"
-                $("#departure").autocomplete({
-                    source: function(request, response) {
-                        // Filtrer les planètes qui commencent par la chaîne entrée
-                        var matches = $.map(data, function(planet) {
-                            if (planet.toLowerCase().startsWith(request.term.toLowerCase())) {
-                                return planet;
-                            }
-                        });
-                        response(matches); // Retourner les résultats filtrés
-                    },
-                    minLength: 2, // Attendre que l'utilisateur écrive 2 caractères avant de proposer des suggestions
-                    autoFocus: true // Pour que le premier élément soit sélectionné automatiquement
-                });
-
-                $("#arrival").autocomplete({
-                    source: function(request, response) {
-                        // Filtrer les planètes qui commencent par la chaîne entrée
-                        var matches = $.map(data, function(planet) {
-                            if (planet.toLowerCase().startsWith(request.term.toLowerCase())) {
-                                return planet;
-                            }
-                        });
-                        response(matches); // Retourner les résultats filtrés
-                    },
-                    minLength: 2, // Attendre que l'utilisateur écrive 2 caractères avant de proposer des suggestions
-                    autoFocus: true // Pour que le premier élément soit sélectionné automatiquement
-                });
-            } else {
-                console.error("Erreur lors de la récupération des planètes.");
-            }
-        });
-    });
-</script>
+<script src="../scripts/AutoPlanet.js"></script>
 
 
